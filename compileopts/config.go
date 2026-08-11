650b6ec60c7133a345979f82e589ebc8ffa8a5f6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-10 21:07:08 -0700

    runtime: keep swept Whippet blocks empty

--- compileopts/config.go
+++ compileopts/config.go
@@ -27,7 +27,7 @@ var libVersions = map[string]int{
 	"bdwgc":        19,
 	"picolibc":     1,
 	"wasmbuiltins": 1,
-	"whippet":      15,
+	"whippet":      16,
 }
 
 // Config keeps all configuration affecting the build in a single struct.
