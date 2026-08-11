0d91204b107e4f9ab3feeeb49e6020286840fd57
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-11 00:33:40 -0700

    runtime: speed up sparse Boehm array marking

--- src/runtime/gc_boehm_array_wasm.go
+++ src/runtime/gc_boehm_array_wasm.go
@@ -5,5 +5,10 @@ package runtime
 import "unsafe"
 
 func boehmArrayDescriptor(layout unsafe.Pointer, elementCount, elementSize uintptr) uintptr {
-	return 0
+	return libgc_make_array_descriptor(
+		uintptr(layout), elementCount, elementSize,
+	)
 }
+
+//export tinygo_runtime_bdwgc_make_array_descriptor
+func libgc_make_array_descriptor(uintptr, uintptr, uintptr) uintptr
